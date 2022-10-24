5c504a2ee002f8b1eb8b3d80f8f49d648cb9faaf
Chedly25 <101675971+Chedly25@users.noreply.github.com>
2022-10-24 10:39:59 +0200

    tp 3

--- app/src/main/java/com/ismin/android/CreateBookActivity.kt
+++ app/src/main/java/com/ismin/android/CreateBookActivity.kt
@@ -1,11 +1,44 @@
 package com.ismin.android
 
-import android.os.Bundle
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 
 class CreateBookActivity : AppCompatActivity() {
+    val EXTRA_BOOK = "extra-book"
+    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_book)
+        val btnSave = findViewById<Button>(R.id.a_create_book_btn_save)
+
+        btnSave.setOnClickListener { view: View? ->
+
+            stopActivityAndReturnResult()
+        }
+        //val intent = Intent(this, CreateBookActivity::class.java)
+        //startForResult.launch(intent)
     }
+    fun stopActivityAndReturnResult() {
+        val titre = findViewById<EditText>(R.id.a_create_book_edt_title).text.toString()
+        val auteur = findViewById<EditText>(R.id.a_create_book_edt_author).text.toString()
+        val date = findViewById<EditText>(R.id.a_create_book_edt_date).text.toString()
+        val bouquin = Book(
+            title = titre,
+            author = auteur,
+            date = date
+        )
+        val returnIntent = Intent()
+        returnIntent.putExtra(EXTRA_BOOK, bouquin)
+        setResult(Activity.RESULT_OK, returnIntent)
+        finish()
+    }
+
 }
--- app/src/main/java/com/ismin/android/MainActivity.kt
+++ app/src/main/java/com/ismin/android/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.ismin.android
-
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    val EXTRA_BOOK = "extra-book"
+    private lateinit var books : Bookshelf
+    private lateinit var txvBook : TextView
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val book = result.data?.getSerializableExtra(EXTRA_BOOK) as Book
+            books.addBook(book)
+            //txvBook.text = book.toString()
+        }
+    }
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnAddBottle = findViewById<Button>(R.id.a_main_btn_create_book)
+        txvBook = findViewById(R.id.a_main_txv_book)
+        btnAddBottle.setOnClickListener { view: View? ->
+            val intent = Intent(this, CreateBookActivity::class.java)
+            // start your next activity
+            startForResult.launch(intent)
+        }
     }
+
 }
+
